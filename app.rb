cc5aa44ff437a592c4d7c0c1aaf20101d7572884
Alexandra Roth <alroth8@gmail.com>
2017-02-20 16:44:02 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,21 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+	get '/newteam' do
+		erb :newteam
+	end
+
+	post '/team' do
+		@teamname = params[:name]
+		@coach = params[:coach]
+		@pointguard = params[:pg]
+		@shootingguard = params[:sg]
+		@smallforward = params[:sf]
+		@powerforward = params[:pf]
+		@center = params[:c]
+		erc :team
+	end
+
+
 
 end
